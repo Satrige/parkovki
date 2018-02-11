f94e3db224ea610a6b7f339a1e96b5b9ef5962fe
satrige <emperor1972@gmail.com>
2018-02-11 21:47:16 +0300

    Add test for inserting new record

---
+++ test/handlers/calendar.spec.js
@@ -0,0 +1,91 @@
+const proxyquire = require('proxyquire').noCallThru();
+
+describe('handlers: calendar', () => {
+  describe('insertNewRecord', () => {
+    it('should resolve with new record object', async () => {
+      const test = {
+        recordInfo: {
+          name: 'Alanis Simonis',
+          email: 'alanis_simonis1959@example.com',
+          date: '31.12.1979',
+          status: 'remoteWork',
+          isDeleted: false,
+          note: '',
+          period: 8,
+        },
+        user: {
+          name: 'Alanis Simonis',
+          email: 'alanis_simonis1959@example.com',
+        },
+        workerHours: 0,
+        toJSONResult: {
+          _id: '123',
+          name: 'Alanis Simonis',
+          email: 'alanis_simonis1959@example.com',
+          date: '31.12.1979',
+          status: 'remoteWork',
+          isDeleted: false,
+          note: '',
+          period: 8,
+        },
+      };
+
+      const expected = {
+        result: {
+          _id: '123',
+          name: 'Alanis Simonis',
+          email: 'alanis_simonis1959@example.com',
+          date: '31.12.1979',
+          status: 'remoteWork',
+          isDeleted: false,
+          note: '',
+          period: 8,
+        },
+        userStatParams: {
+          email: 'alanis_simonis1959@example.com',
+          date: new Date('12.31.1979'),
+        },
+        saveNewRecordParams: {
+          __v: 0,
+          name: 'Alanis Simonis',
+          email: 'alanis_simonis1959@example.com',
+          date: new Date('12.31.1979'),
+          status: 'remoteWork',
+          isDeleted: false,
+          note: '',
+          period: 8,
+        },
+        userQuery: {
+          email: 'alanis_simonis1959@example.com',
+        },
+      };
+
+      const calendarHandler = proxyquire('handlers/calendar', {
+        'models/calendar': {
+          getSingleUserStat: async (params) => {
+            expect(params).to.be.deep.equal(expected.userStatParams);
+
+            return test.workerHours;
+          },
+          saveNewRecord: async (recordInfo) => {
+            expect(recordInfo).to.be.deep.equal(expected.saveNewRecordParams);
+
+            return {
+              toJSON: () => test.toJSONResult,
+            };
+          },
+        },
+        'handlers/users': {
+          findUser: async (params) => {
+            expect(params).to.be.deep.equal(expected.userQuery);
+
+            return test.user;
+          },
+        },
+      });
+
+      await expect(calendarHandler.insertNewRecord(test.recordInfo))
+        .to.eventually.be.deep.equal(expected.result);
+    });
+  });
+});
